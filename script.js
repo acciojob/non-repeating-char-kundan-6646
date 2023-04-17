397846c32b4ece21515fab4787402c8b1882948a
Kundan Kumar <59140242+kundan-6646@users.noreply.github.com>
2023-04-17 21:31:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+var str = prompt("Enter the number of elements");
+alert(getChar(str));
+
+function getChar(word) {
+	let idx = 0;
+	while(idx < word.length) {
+		let j = idx;
+		let cc = word.charAt(idx);
+		while(j < word.length && cc == word.charAt(j)) j++;
+
+		if(j-idx == 1) return cc;
+		idx = j;
+	}
+
+	return '';
+}
